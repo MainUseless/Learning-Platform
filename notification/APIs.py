4ba6128c254f6bb83e661743e08b28280a9b7058
MainUseless <mostafa777444333@gmail.com>
2024-05-12 00:47:09 +0300

    added jwt in notification

--- notification/APIs.py
+++ notification/APIs.py
@@ -1,3 +1,4 @@
+import jwt
 from flask import Flask, request, jsonify
 import sqlite3
 from flask_cors import CORS
@@ -8,43 +9,46 @@ CORS(app)
 
 @app.route('/notification', methods=['POST'])
 def add_notification():
-  data = request.get_json()
-  stud_id = data['stud_id']
-  message = data['message']
+	data = request.get_json()
+	token_body = decode_token(request)
+	stud_id = token_body['stud_id']
+	message = data['message']
 
-  conn = sqlite3.connect('notification.db')
-  c = conn.cursor()
+	conn = sqlite3.connect('notification.db')
+	c = conn.cursor()
 
-  stmt = '''INSERT INTO notifications (stud_id, message, is_read) VALUES (?, ?, 0)'''
-  c.execute(stmt, (stud_id, message))
+	stmt = '''INSERT INTO notifications (stud_id, message, is_read) VALUES (?, ?, 0)'''
+	c.execute(stmt, (stud_id, message))
 
-  conn.commit()
-  conn.close()
+	conn.commit()
+	conn.close()
 
-  return jsonify({'message': 'Notification added'}), 201
+	return jsonify({'message': 'Notification added'}), 201
 
 @app.route('/notifications', methods=['GET'])
 def get_unread_notifications():
-  stud_id = request.args.get('stud_id')
-  conn = sqlite3.connect('notification.db')
-  c = conn.cursor()
+	token_body = decode_token(request)
+	stud_id = token_body['stud_id']
+	conn = sqlite3.connect('notification.db')
+	c = conn.cursor()
 
-  stmt = '''SELECT * FROM notifications WHERE stud_id = ? AND is_read = 0'''
-  notifications = c.execute(stmt, (stud_id,)).fetchall()
+	stmt = '''SELECT * FROM notifications WHERE stud_id = ? AND is_read = 0'''
+	notifications = c.execute(stmt, (stud_id,)).fetchall()
 
-  unread_notifications = [{'id': n[1], 'message': n[2]} for n in notifications]
+	unread_notifications = [{'id': n[1], 'message': n[2]} for n in notifications]
 
-  update_stmt = '''UPDATE notifications SET is_read = 1 WHERE stud_id = ? AND is_read = 0'''
-  c.execute(update_stmt, (stud_id,))
+	update_stmt = '''UPDATE notifications SET is_read = 1 WHERE stud_id = ? AND is_read = 0'''
+	c.execute(update_stmt, (stud_id,))
 
-  conn.commit()
-  conn.close()
+	conn.commit()
+	conn.close()
 
-  return jsonify(unread_notifications)
+	return jsonify(unread_notifications)
 
 @app.route('/unreadnotification', methods=['GET'])
 def get_unread_notification_count():
-    stud_id = request.args.get('stud_id')
+    token_body = decode_token(request)
+    stud_id = token_body['stud_id']
     
     conn = sqlite3.connect('notification.db')
     c = conn.cursor()
@@ -53,6 +57,21 @@ def get_unread_notification_count():
     unread_count = c.execute(stmt, (stud_id,)).fetchone()[0]
 
     return "You Have " + str(unread_count) + " Unread Notifications"
+
+@app.route('/', methods=['GET'])
+def test():
+    return "Hello World!"
+
+def decode_token(request):
+    encoded_token = request.headers.get('Authorization', None)
+    encoded_token = encoded_token.split(' ')[1]
+    try:
+      # Set verify_signature to False to skip signature validation
+      token = jwt.decode(encoded_token, options={"verify_signature": False}) 
+      return token
+    except Exception as e:
+      print(f"Error decoding JWT: {e}")
+      return None
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8082)
